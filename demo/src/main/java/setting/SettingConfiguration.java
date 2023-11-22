6633a6a5be12f18b0060f8eddeed34364476d589
jyolo <438608613@qq.com>
2023-11-22 20:17:44 +0800

    tt

---
+++ demo/src/main/java/setting/SettingConfiguration.java
@@ -0,0 +1,26 @@
+package com.obiscr.chatgpt.settings;
+
+/**
+ * @author Wuzi
+ */
+public class SettingConfiguration {
+
+    public enum SettingURLType {
+        //
+        OFFICIAL,
+
+        //
+        CUSTOMIZE,
+    }
+
+    public enum SettingProxyType {
+        //
+        DIRECT,
+
+        //
+        HTTP,
+
+        //
+        SOCKS
+    }
+}
