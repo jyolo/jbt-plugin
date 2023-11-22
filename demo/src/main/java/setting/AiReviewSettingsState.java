80d7bca9a1f838d12768061973c55f7ffc842155
jyolo <438608613@qq.com>
2023-11-22 20:08:05 +0800

    tt

---
+++ demo/src/main/java/setting/AiReviewSettingsState.java
@@ -0,0 +1,41 @@
+package com.qianliuAiUi.settings;
+
+import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.components.PersistentStateComponent;
+import com.intellij.openapi.components.State;
+import com.intellij.openapi.components.Storage;
+import com.intellij.util.xmlb.XmlSerializerUtil;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+@State(
+        name = "com.aireview.settings.AiReviewSettingState",
+        storages = @Storage("AiReviewSettingsPlugin.xml")
+)
+public class AiReviewSettingsState implements PersistentStateComponent<AiReviewSettingsState> {
+
+    public static String toolWindowName = "千流AI";
+    public Boolean enableAiReview = false;
+    public String shortcutKeys = "alt pressed R";
+    public Integer maxDelayTime = 2000;
+    public Integer delayTime = 10;
+    public Boolean reviewIng = false;
+
+
+    public static AiReviewSettingsState getInstance(){
+        return ApplicationManager.getApplication().getService(AiReviewSettingsState.class);
+    }
+
+    @Nullable
+    @Override
+    public AiReviewSettingsState getState() {
+        return this;
+    }
+
+    @Override
+    public void loadState(@NotNull AiReviewSettingsState state) {
+        XmlSerializerUtil.copyBean(state, this);
+    }
+
+
+}
---
+++ demo/src/main/java/setting/SettingsPanel.java
@@ -0,0 +1,97 @@
+package com.qianliuAiUi.settings;
+
+import com.intellij.openapi.Disposable;
+import com.intellij.openapi.options.Configurable;
+import com.intellij.openapi.options.ConfigurationException;
+import com.intellij.ui.TitledSeparator;
+import org.jetbrains.annotations.Nullable;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class SettingsPanel implements Configurable, Disposable {
+
+    AiReviewSettingsState settingsState = AiReviewSettingsState.getInstance();
+    private JPanel mainPanel;
+    private JLabel subTitle;
+    private JPanel titledBorderBox;
+    private JCheckBox enableAiReview;
+    private JTextField delayTime;
+    private JFormattedTextField shortcutKeys;
+    public String getDisplayName() {
+        return "千流AI代码Review";
+    }
+
+    @Override
+    public void dispose() {}
+
+    @Override
+    public @Nullable JComponent createComponent() {
+        delayTime.setText(String.valueOf(settingsState.delayTime));
+        enableAiReview.setSelected(settingsState.enableAiReview);
+
+        shortcutKeys.setEditable(false);
+        return mainPanel;
+    }
+
+    @Override
+    public boolean isModified() {
+        boolean modified = false;
+        modified = checkInteger(settingsState.delayTime, delayTime);
+        if (modified){
+            return modified;
+        }
+
+        modified = checkBoolean(settingsState.enableAiReview, enableAiReview);
+        if (modified){
+            return modified;
+        }
+        return false;
+    }
+
+    @Override
+    public void apply() throws ConfigurationException {
+        settingsState.enableAiReview = enableAiReview.isSelected();
+        settingsState.delayTime = Integer.valueOf(delayTime.getText());
+        if (settingsState.delayTime < 2) {
+            settingsState.delayTime = 2;
+        } else if (settingsState.delayTime > 1440) {
+            settingsState.delayTime = 1440;
+        }
+    }
+
+    private boolean checkBoolean(boolean settingsParams, JCheckBox formParams) {
+        /**
+         * is equals
+         */
+        if (formParams != null) {
+            return formParams.isSelected() != settingsParams;
+        }
+        return false;
+    }
+
+    private boolean checkInteger(long settingsParams, JTextField formParams) {
+        /**
+         * is equals
+         */
+        String text = formParams.getText();
+        if (text == null || text.isEmpty()) {
+            return false;
+        }
+        try{
+            if (Integer.valueOf(text) == settingsParams){
+                return false;
+            }
+        }catch (NumberFormatException e){
+            return false;
+        }
+
+        return true;
+    }
+
+    private void createUIComponents() {
+        titledBorderBox = new JPanel(new BorderLayout());
+        TitledSeparator tsUrl = new TitledSeparator("千流AI代码Review");
+        titledBorderBox.add(tsUrl,BorderLayout.CENTER);
+    }
+}
