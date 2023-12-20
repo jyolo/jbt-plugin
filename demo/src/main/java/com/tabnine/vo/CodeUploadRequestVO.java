133f77f2005840e8f14ecd38b0f93da6986bb2ad
jyolo <438608613@qq.com>
2023-12-20 21:57:03 +0800

    tt

---
+++ demo/src/main/java/com/tabnine/vo/CodeUploadRequestVO.java
@@ -0,0 +1,82 @@
+package tabCompletion.vo;
+
+import java.util.Map;
+
+public class CodeUploadRequestVO extends RequestVO {
+
+    String response_id;
+    String languageId;
+    String prompts;
+    String code_completions_text;
+    Boolean is_code_completion;
+    String ide;
+    String current_model;
+    ExtraVO extra_kwargs;
+    private Map<String, Object> server_extra_kwargs;
+
+    public CodeUploadRequestVO() {
+    }
+
+    @Override
+    public String toString() {
+        return "RequestVO{" +
+                "response_id='" + response_id + '\'' +
+                ", languageId=" + languageId +
+                ", is_code_completion=" + is_code_completion +
+                ", prompts=" + prompts +
+                ", code_completions_text=" + code_completions_text +
+                ", ide='" + ide + '\'' +
+                ", extra_kwargs='" + extra_kwargs + '\'' +
+                ", server_extra_kwargs='" + server_extra_kwargs + '\'' +
+                '}';
+    }
+
+    public void setExtra(ExtraVO extra) {
+        this.extra_kwargs = extra;
+    }
+
+
+    public CodeUploadRequestVO(String response_id, String languageId, String prompts, String code_completions_text, boolean is_code_completion, String ide, String current_model) {
+        this.response_id = response_id;
+        this.languageId = languageId;
+        this.prompts = prompts;
+        this.code_completions_text = code_completions_text;
+        this.is_code_completion = is_code_completion;
+        this.ide = ide;
+        this.current_model = current_model;
+    }
+
+    public void setServer_extra_kwargs(Map<String, Object> server_extra_kwargs) {
+        this.server_extra_kwargs = server_extra_kwargs;
+    }
+
+    public void setResponse_id(String response_id) {
+        this.response_id = response_id;
+    }
+
+    public String getResponse_id() {return this.response_id;}
+
+    public void setLanguageId(String languageId) {
+        this.languageId = languageId;
+    }
+
+    public void setCode_completions_text(String code_completions_text) {
+        this.code_completions_text = code_completions_text;
+    }
+
+    public void setIs_code_completion(Boolean is_code_completion) {
+        this.is_code_completion = is_code_completion;
+    }
+
+    public void setIde(String ide) {
+        this.ide = ide;
+    }
+
+    public void setPrompts(String prompts) {
+        this.prompts = prompts;
+    }
+
+    public void setCurrent_model(String current_model) {
+        this.current_model = current_model;
+    }
+}
---
+++ demo/src/main/java/com/tabnine/vo/ResultEntry.java
@@ -0,0 +1,63 @@
+package tabCompletion.vo;
+
+import tabCompletion.binary.requests.autocomplete.CompletionMetadata;
+import tabCompletion.general.CompletionKind;
+import tabCompletion.intellij.completions.Completion;
+
+public class ResultEntry implements Completion {
+    public String new_prefix;
+    public String old_suffix;
+    public String new_suffix;
+    public CompletionMetadata completion_metadata;
+
+    @Override
+    public boolean isSnippet() {
+        if (this.completion_metadata == null) {
+            return false;
+        }
+
+        return this.completion_metadata.getCompletion_kind() == CompletionKind.Snippet;
+    }
+
+    public String getNew_prefix() {
+        return new_prefix;
+    }
+
+    public void setNew_prefix(String new_prefix) {
+        this.new_prefix = new_prefix;
+    }
+
+    public String getOld_suffix() {
+        return old_suffix;
+    }
+
+    public void setOld_suffix(String old_suffix) {
+        this.old_suffix = old_suffix;
+    }
+
+    public String getNew_suffix() {
+        return new_suffix;
+    }
+
+    public void setNew_suffix(String new_suffix) {
+        this.new_suffix = new_suffix;
+    }
+
+    public CompletionMetadata getCompletion_metadata() {
+        return completion_metadata;
+    }
+
+    public void setCompletion_metadata(CompletionMetadata completion_metadata) {
+        this.completion_metadata = completion_metadata;
+    }
+
+    @Override
+    public String toString() {
+        return "ResultEntry{" +
+                "new_prefix='" + new_prefix + '\'' +
+                ", old_suffix='" + old_suffix + '\'' +
+                ", new_suffix='" + new_suffix + '\'' +
+                ", completion_metadata=" + completion_metadata +
+                '}';
+    }
+}
